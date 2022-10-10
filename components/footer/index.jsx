abfd4eb17db9f1f52ce0f8b769a4ef638daadc8a
acz369 <andrea.calderonz@udea.edu.co>
2022-10-09 22:51:35 -0500

    cambios visuales
    Se agregron los links de las páginas creadas, se modificaron botones y estilos de la página principal

--- components/footer/index.jsx
+++
@@ -1,6 +0,0 @@
-export const Footer = () => (
-  <div className='header w-full bg-blue-200'>
-    <h1 className='text-white'>Copyright &copy;</h1>
-    <h1 className='text-white'>2022</h1>
-  </div>
-);
--- pages/home/home.tsx
+++ pages/home/home.tsx
@@ -1,21 +1,72 @@
 import { Footer } from '@components/footer';
 import { Header } from '@components/header';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Link from 'next/link';
 
 export const InitialPage = () => (
   <>
     <Header />
-    <div className='header flex w-full'>
-      <div>Contenido</div>
-      <div className='max-w-sm rounded overflow-hidden shadow-lg m-4 bg-blue-100'>
+    <div className='text-center text-blue-200 font-bold text-3xl py-4'>
+      Gestiona tu viaje
+    </div>
+    <div className='header flex justify-center w-full pb-6'>
+      <div className='max-w-sm rounded overflow-hidden shadow-lg m-4 bg-blue-50'>
         <div className='px-6 py-4'>
-          <div className='font-bold text-xl mb-2 text-white'>
-            <Link href='/travel/travel'>Ver viaje</Link>
+          <img src='/travel.jpg' alt='travel' width='200' height='200' />
+          <div className='font-bold text-xl mb-2 text-blue-200'>
+            <p className='pt-2'>Viaje</p>
           </div>
-          <p className='text-white text-base'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
+          <p className='text-blue-200'>
+            Crear un <Link href='/travel/CreateTravel'><span className='underline'>viaje</span></Link>
+          </p>
+          <p className='text-blue-200'>
+            Editar un <Link href='/travel/EditTravel'><span className='underline'>viaje</span></Link>
+          </p>
+          <p className='text-blue-200'>
+            Agregar un <Link href='/travel/addUser'><span className='underline'>usuario</span></Link>
+          </p>
+          <p className='text-blue-200'>
+            Ver <Link href='/travel/travel'><span className='underline'>viaje</span></Link>
+          </p>
+        </div>
+      </div>
+      <div className='max-w-sm rounded overflow-hidden shadow-lg m-4 bg-blue-50'>
+        <div className='px-6 py-4'>
+          <img src='/travel.jpg' alt='travel' width='200' height='200' />
+          <div className='font-bold text-xl mb-2 text-blue-200'>
+            <p className='pt-2'>Actividad</p>
+          </div>
+          <p className='text-blue-200'>
+            Crear una <Link href='/activities/CreateActivities'>actividad</Link>
+          </p>
+        </div>
+      </div>
+      <div className='max-w-sm rounded overflow-hidden shadow-lg m-4 bg-blue-50'>
+        <div className='px-6 py-4'>
+          <img src='/travel.jpg' alt='travel' width='200' height='200' />
+          <div className='font-bold text-xl mb-2 text-blue-200'>
+            <p className='pt-2'>Presupuesto</p>
+          </div>
+          <p className='text-blue-200'>
+            Crear una <Link href='/activities/CreateEntries'>entrada</Link>
+          </p>
+          <p className='text-blue-200'>
+            Editar una <Link href='/activities/EditEntries'>entrada</Link>
+          </p>
+        </div>
+      </div>
+      <div className='max-w-sm rounded overflow-hidden shadow-lg m-4 bg-blue-50'>
+        <div className='px-6 py-4'>
+          <img src='/travel.jpg' alt='travel' width='200' height='200' />
+          <div className='font-bold text-xl mb-2 text-blue-200'>
+            <p className='pt-2'>Destino</p>
+          </div>
+          <p className='text-blue-200'>
+            Crear un <Link href='/destination/CreateDestination'>destino</Link>
+          </p>
+          <p className='text-blue-200'>
+            Editar un <Link href='/destination/EditDestination'>destino</Link>
           </p>
         </div>
       </div>
